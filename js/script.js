bf6381b7fb9e85b59849087ea3264cfd558b205b
Fabiano Gabriel Tassotti <fabianotassotti@hotmail.com>
2021-07-18 14:55:36 -0300

    Countdown criado

--- js/script.js
+++ js/script.js
@@ -11,15 +11,13 @@ cadastro.addEventListener('submit', (e) => {
     if (!email.includes("@")) {
         alert("Email inválido.");
     } else
-    if (JSON.parse(localStorage.getItem('usuario')).email == email) {
-        alert("Você já está cadastrado no Local Storage!!!!!.");
+    if (checkLocalStorage() == true) {
+        alert("Você já está cadastrado no Local Storage!!!!!");
     } else {
         let data = {
             nome,
             email,
         }
-
-        console.log(data);
 
         let convertData = JSON.stringify(data);
 
@@ -27,4 +25,42 @@ cadastro.addEventListener('submit', (e) => {
 
         alert("Nós informaremos as novas ofertas, Obrigado por se Cadastrar!");
     }
-})
+
+    function checkLocalStorage() {
+        try {
+            if (JSON.parse(localStorage.getItem('usuario')).email == email) {
+                return true;
+            }
+        } catch {
+            return false
+        }
+    }
+});
+
+countdown("Aug 5, 2021 23:59:59", "countdown01");
+countdown("Aug 5, 2021 23:59:59", "countdown02");
+countdown("Aug 5, 2021 23:59:59", "countdown03");
+countdown("Aug 5, 2021 23:59:59", "countdown04");
+countdown("Aug 5, 2021 23:59:59", "countdown05");
+
+function countdown(end, id) {
+    var countDownDate = new Date(end).getTime();
+    var x = setInterval(function() {
+        var now = new Date().getTime();
+
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById(id).innerHTML = days + "D " + hours + ":" +
+            minutes + ":" + seconds;
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById(id).innerHTML = "EXPIRED";
+        }
+    }, 1000);
+}
